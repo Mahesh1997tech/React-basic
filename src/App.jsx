c76f5d732852fb04c9d2c01e9bfa0874538c4c98
maheshwari.b <maheshwari.maadhu@gamil.com>
2025-02-08 21:02:39 +0530

    react simple project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function app(){
+  return(
+  <div>
+        <h1>My App</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quaerat, rem dolorem minima, aliquid, consequatur fugiat magnam odit quod nihil similique fuga dicta culpa labore officia! Dolorum fugit repudiandae provident.
+        </p>
+  </div>)
+}
+export default app;
---
+++ src/Mycomponent.jsx
@@ -0,0 +1,13 @@
+function Mycomponent(){
+    return(
+        <div className="mycomponent">
+            <div className="mycomponent_header">
+                <h2>MY COMPONENT</h2>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Mycomponent
