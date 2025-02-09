676a72a7ea52592e9a76b48f1b0a25be68970df6
maheshwari.b <maheshwari.maadhu@gamil.com>
2025-02-09 16:15:17 +0530

    add useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -136,17 +136,61 @@
 
 
 
-import react, { useState } from "react";
-import Profile from "./components/Profile";
-import Footer from "./components/Footer";
+// import react, { useState } from "react";
+// import Profile from "./components/Profile";
+// import Footer from "./components/Footer";
+
+// function App(){
+
+//         return (
+//         <>
+//         <Profile></Profile>
+//         <Footer></Footer>
+//         </>
+//     )                       
+// }
+//  export default App;    
+
+
+
+import React, { useState } from "react";
+
+import { useReducer } from "react";
+
 
 function App(){
+    const initialState={count:0}
 
-        return (
+    const reducer=(state,action)=>{
+        switch(action.type){
+            case 'increase':{
+                return{count:state.count+1}
+        }
+            case 'decrease':{
+                return{count:state.count-1}
+            }
+    case 'input':{  
+        return{count:action.payload}
+    }   
+            default:{
+        return state
+            }
+        }
+    }
+
+   const[state, dispatch]= useReducer(reducer, initialState)//takes two arguments 1 is function and 2nd is initial state
+
+    // const[count,setCount]=useState(0)
+    return(
         <>
-        <Profile></Profile>
-        <Footer></Footer>
+
+        <h1>{state.count}</h1>
+        <button onClick={()=>dispatch({type:'increase'})}>increase</button>
+        <button onClick={()=>dispatch({type:'decrease'})}>decrease</button><br></br>
+        
+        <input value={state.count} onChange={(e)=>dispatch({type:'input',payload:Number(e.target.value)})} type="number"  name="" id="" />
+
         </>
-    )                       
+    )
 }
- export default App;    
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
-import ContextProvider from './context/AppContext.jsx'
+// import ContextProvider from './context/AppContext.jsx'
 // import './App.css'
 // import './project1/Home.css' 
 // import Mycomponent from './Mycomponent.jsx'
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
     {/* <Mycomponent></Mycomponent> */}
     {/* <Home></Home> */}
 <App></App>
-<ContextProvider></ContextProvider>
+{/* <ContextProvider></ContextProvider> */}
 
     </div>
   
